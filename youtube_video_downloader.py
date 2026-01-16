06b705c74c1e864e0367360543c5e1d7a80bd0a8
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2026-01-16 13:30:47 +0530

    cookie issue bug not fixed

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -2,13 +2,23 @@ import subprocess
 from remove_file_from_folder import remove_video_files_from_folder
 from pathlib import Path
 
+
 def download_youtube_video(youtube_url: str, yt_video_folder: Path) -> None:
-    '''
+    """
     Downloads a YouTube video to the specified folder.
-    '''
+    """
     remove_video_files_from_folder(yt_video_folder)
 
+    command = [
+        "yt-dlp",
+        "--cookies-from-browser",
+        "edge",
+        "-o",
+        f"{yt_video_folder}/%(title)s",
+        youtube_url,
+    ]
+
     subprocess.run(
-        f"yt-dlp -f bestvideo+bestaudio -o {yt_video_folder}/%(title)s {youtube_url}",
-        shell=True,
+        command,
+        shell=False,
     )
